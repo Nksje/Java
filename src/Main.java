be73e925a3070860173b75d97251a95185b89e84
Daniil Bondar <daniil.bondar49@gmail.com>
2023-06-25 19:21:59 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,64 @@
+// import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello");
+
+        // This is a comment
+        /*
+         * This
+         * is
+         * a
+         * comment
+         */
+
+        /* 1. VARIABLES */
+        // int x = 21;
+        // long x = 10212121212121L; // initialization
+        // double y = 3.13;
+        // float z = 3.14f;
+        // boolean z = false;
+        // boolean a = true;
+        // char symbol = '@';
+        // String name = "Daniil";
+
+        // System.out.println("My number is: " + x);
+        // System.out.println("My number is: " + y);
+        // System.out.println("Hello " + name);
+
+        // String x = "Water";
+        // String y = "Kool-Aid";
+        // String temp;
+
+        // temp = x;
+        // x = y;
+        // y = temp;
+
+        // System.out.println("x: " + x);
+        // System.out.println("y: " + y);
+        // System.out.println(temp);
+
+        /* 2. USER INPUT */
+
+        // Scanner scanner = new Scanner(System.in);
+
+        // System.out.println("What is your name? ");
+        // String name = scanner.nextLine();
+        // System.out.println("How old are you? ");
+        // int age = scanner.nextInt();
+        // scanner.nextLine();
+        // System.out.println("What is your favorite food?");
+        // String food = scanner.nextLine();
+        // System.out.println("Hello " + name);
+        // System.out.println("You are " + age + " years old");
+        // System.out.println("Your like " + food);
+
+        /* 3. Expressions */
+
+        // double friends = 10;
+
+        // friends = friends / 3;
+
+        // System.out.println(friends);
+    }
+}
