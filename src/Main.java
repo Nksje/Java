0858e8a3897efd92eaaf73b9fc97ccfc97bcc7ce
Daniil Bondar <daniil.bondar49@gmail.com>
2023-06-28 15:36:12 +0300

    GUI

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+
 // import java.util.Scanner;
+import javax.swing.JOptionPane;
 
 public class Main {
     public static void main(String[] args) {
@@ -60,5 +62,18 @@ public class Main {
         // friends = friends / 3;
 
         // System.out.println(friends);
+
+        /* 4. GUI intro */
+        String name = JOptionPane.showInputDialog("Enter your name: ");
+
+        JOptionPane.showMessageDialog(null, "Hello " + name);
+
+        int age = Integer.parseInt(JOptionPane.showInputDialog("Enter your age: "));
+
+        JOptionPane.showMessageDialog(null, "You are " + age + " years old!");
+
+        double height = Double.parseDouble(JOptionPane.showInputDialog("Enter your height: "));
+
+        JOptionPane.showMessageDialog(null, "Your height is " + height + "cm");
     }
 }
